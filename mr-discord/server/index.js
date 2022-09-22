75105456f923fb890b195a3c86d0977e34252800
Luz Iferato <105192630+k37z3r@users.noreply.github.com>
2022-09-22 23:06:16 +0200

    Update index.js

--- mr-discord/server/index.js
+++ mr-discord/server/index.js
@@ -2,17 +2,54 @@ import * as alt from 'alt';
 import axios from 'axios';
 import fs from 'fs';
 import util from 'util';
-import Discord from 'discord.js'
+import Discord from 'discord.js';
+import dotenv from 'dotenv';
+import mysql from 'mysql';
 const { Client, GatewayIntentBits } = Discord;
 const discordClient = new Client({ intents: [ GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers, ] });
 import { _CONFIG } from '../config/config.js';
 import { _L } from '../language/language.js';
 let WhitelistUpdate;
-var onlineusers = [];
+var CurrentOnlineUsers = [];
 var WhiteList = [];
-var AdminList = [];
-var ModList = [];
-var SupporterList = [];
+//-----------------auszulagernde funktion
+function mysql_callback(sql, cb){
+	con.query(sql, (err, rows, fields) => {
+		if (err){
+			throw err;
+			process.exit(0);
+		}
+		return cb(rows[0].COUNTER);
+	});
+}
+//-------------------------
+dotenv.config();
+const neededValues = [
+    'MYSQL_HOST',
+    'MYSQL_USER',
+    'MYSQL_PASSWORD',
+    'MYSQL_DATABASE',
+	'DISCORD_APP_ID',
+	'DISCORD_SERVER_ID',
+	'DISCORD_TOKEN',
+	'DISCORD_CITIZEN_ID',
+	'DISCORD_ADMIN_ID',
+	'DISCORD_MOD_ID',
+	'DISCORD_SUPPORTER_ID',
+	'DISCORD_STATUS_CHANNEL'
+];
+neededValues.forEach((value) => {
+    if (!(value in process.env)) {
+        alt.log(`the needed key '${value}' is missing in .env file`);
+        process.exit(0);
+    }
+});
+var con = mysql.createConnection({
+	host: process.env.MYSQL_HOST,
+	user: process.env.MYSQL_USER,
+	password: process.env.MYSQL_PASSWORD,
+	database: process.env.MYSQL_DATABASE
+});
 
 discordClient.on('guildMemberUpdate', (oldMember, newMember) => {
 	refreshWhitelist();
@@ -23,7 +60,7 @@ discordClient.on("messageCreate", (message) => {
 
 	if (message.content.startsWith(`${_CONFIG.DISCORD_PREFIX}whitelist`)) {
 		let userlist='';
-		message.guild.roles.cache.get(_CONFIG.DISCORD_CITIZEN_ID).members.forEach(m=>{
+		message.guild.roles.cache.get(process.env.DISCORD_CITIZEN_ID).members.forEach(m=>{
 			let sf_citizen = (element) => element == m.user.id;
 			let index = WhiteList.findIndex(sf_citizen);
 			userlist += '\n' + m.user.username;
@@ -38,15 +75,21 @@ discordClient.on("messageCreate", (message) => {
 	}
 });
 alt.on('playerConnect', (player) => {
-	alt.emitClient(player, 'bao:discord:set_discord_app_id',  _CONFIG.DISCORD_APP_ID);
+	alt.emitClient(player, 'mr-core:discord:set_discord_app_id',  process.env.DISCORD_APP_ID);
 });
 alt.on('playerDisconnect', (player) => {
-	let server = discordClient.guilds.cache.get(_CONFIG.DISCORD_SERVER_ID);
-	server.channels.cache.get(_CONFIG.DISCORD_STATUS_CHANNEL).send(_L("leaved", [getDiscordNameById(player)]));
-	if (getOnlineUsersById(player) > -1)
-		onlineusers.splice(getOnlineUsersById(player), 1);
+	let server = discordClient.guilds.cache.get(process.env.DISCORD_SERVER_ID);
+	//server.channels.cache.get(process.env.DISCORD_STATUS_CHANNEL).send(_L("leaved", [getDiscordNameById(player)]));
+	if (getOnlineUsersById(player.id) > -1)
+		CurrentOnlineUsers.splice(getOnlineUsersById(player.id), 1);
+	con.query("UPDATE users SET id = NULL WHERE id = " + con.escape(player.id), function(err, rows, fields){
+		if (err){
+			throw err.code;
+			process.exit(0);
+		}
+	});
 });
-alt.onClient('bao:discord:token', async (player, token) => {
+alt.onClient('mr-core:discord:token', async (player, token) => {
 	const request = await axios.get('https://discordapp.com/api/users/@me', {
 		'headers': {
 			'Content-Type': 'application/x-www-form-urlencoded',
@@ -63,28 +106,41 @@ alt.onClient('bao:discord:token', async (player, token) => {
         player.kick(_L("no_whitelisted", [_CONFIG.DISCORD_INVITE_LINK]));
         return;
 	}
-	onlineusers.push({id: player, dsid: request.data.id, username: request.data.username});
-
-	let server = discordClient.guilds.cache.get(_CONFIG.DISCORD_SERVER_ID);
-	server.channels.cache.get(_CONFIG.DISCORD_STATUS_CHANNEL).send(_L("joined", [getDiscordNameById(player)]));
+	mysql_callback('SELECT count(*) AS COUNTER FROM users WHERE dsid=' + con.escape(request.data.id), function(result){
+		if(result == 0){
+			con.query("INSERT INTO users (id, dsid, username) VALUES (" + con.escape(player.id) + ", " + con.escape(request.data.id) + ", " + con.escape(request.data.username) + ")", function(err, rows, fields){
+				if (err){
+					throw err.code;
+					process.exit(0);
+				}
+			});
+		}
+		else{
+			con.query("UPDATE users SET id = " + con.escape(player.id) + " WHERE dsid = " + con.escape(request.data.id), function(err, rows, fields){
+				if (err){
+					throw err.code;
+					process.exit(0);
+				}
+			});
+		}
+	});
+	let server = discordClient.guilds.cache.get(process.env.DISCORD_SERVER_ID);
+	//server.channels.cache.get(process.env.DISCORD_STATUS_CHANNEL).send(_L("joined", [getDiscordNameById(player)]));
 });
 
 export function refreshWhitelist(){
-	let server = discordClient.guilds.cache.get(_CONFIG.DISCORD_SERVER_ID);
+	let server = discordClient.guilds.cache.get(process.env.DISCORD_SERVER_ID);
 	server.members.fetch().then(members => {
 		members.forEach(member =>{
-			let isCitizen = member.roles.cache.has(_CONFIG.DISCORD_CITIZEN_ID);
+			let isCitizen = member.roles.cache.has(process.env.DISCORD_CITIZEN_ID);
 			let sf_citizen = (element) => element == member.user.id;
 			let index = WhiteList.findIndex(sf_citizen);
-			let isAdmin = member.roles.cache.has(_CONFIG.DISCORD_ADMIN_ID);
+			let isAdmin = member.roles.cache.has(process.env.DISCORD_ADMIN_ID);
 			let sf_Admin = (element) => element == member.user.id;
-			let Adminindex = AdminList.findIndex(sf_Admin);
-			let isMod = member.roles.cache.has(_CONFIG.DISCORD_MOD_ID);
+			let isMod = member.roles.cache.has(process.env.DISCORD_MOD_ID);
 			let sf_Mod = (element) => element == member.user.id;
-			let Modindex = ModList.findIndex(sf_Mod);
-			let isSupporter = member.roles.cache.has(_CONFIG.DISCORD_SUPPORTER_ID);
+			let isSupporter = member.roles.cache.has(process.env.DISCORD_SUPPORTER_ID);
 			let sf_Supporter = (element) => element == member.user.id;
-			let Supporterindex = SupporterList.findIndex(sf_Supporter);
 			if (_CONFIG.WHITELIST){
 				if (!isCitizen){
 					if (index > -1){
@@ -110,126 +166,92 @@ export function refreshWhitelist(){
 					alt.log(_L("log_whitelist_add",[member.user.username]));
 				}
 			}
+			mysql_callback('SELECT count(*) AS COUNTER FROM users WHERE dsid=' + con.escape(member.user.id), function(result){
+				if(result > 0){
+					if (!isAdmin){
+						con.query('UPDATE users SET isAdmin="no" WHERE dsid=' + con.escape(member.user.id), (err, rows, fields) => {
+							if (err){
+								throw err;
+								process.exit(0);
+							}
+						});
+					}
+					else if (isAdmin){
+						con.query('UPDATE users SET isAdmin="yes" WHERE dsid=' + con.escape(member.user.id), (err, rows, fields) => {
+							if (err){
+								throw err;
+								process.exit(0);
+							}
+						});
+					}
 
-			if (!isAdmin){
-				if (Adminindex > -1){
-					AdminList.splice(adminindex, 1);
-					alt.log(_L("log_adminlist_del",[member.user.username]));
-				}
-			}
-			else if (isAdmin){
-				if (Adminindex <= -1){
-					AdminList.push(member.user.id);
-					alt.log(_L("log_adminlist_add",[member.user.username]));
-				}
-			}
+					if (!isMod){
+						con.query('UPDATE users SET isMod="no" WHERE dsid=' + con.escape(member.user.id), (err, rows, fields) => {
+							if (err){
+								throw err;
+								process.exit(0);
+							}
+						});
+					}
+					else if (isMod){
+						con.query('UPDATE users SET isMod="yes" WHERE dsid=' + con.escape(member.user.id), (err, rows, fields) => {
+							if (err){
+								throw err;
+								process.exit(0);
+							}
+						});
+					}
 
-			if (!isMod){
-				if (Modindex > -1){
-					ModList.splice(Modindex, 1);
-					alt.log(_L("log_modlist_del",[member.user.username]));
+					if (!isSupporter){
+						con.query('UPDATE users SET isSupporter="no" WHERE dsid=' + con.escape(member.user.id), (err, rows, fields) => {
+							if (err){
+								throw err;
+								process.exit(0);
+							}
+						});
+					}
+					else if (isSupporter){
+						con.query('UPDATE users SET isSupporter="yes" WHERE dsid=' + con.escape(member.user.id), (err, rows, fields) => {
+							if (err){
+								throw err;
+								process.exit(0);
+							}
+						});
+					}
 				}
-			}
-			else if (isMod){
-				if (Modindex <= -1){
-					ModList.push(member.user.id);
-					alt.log(_L("log_modlist_add",[member.user.username]));
-				}
-			}
-
-			if (!isSupporter){
-				if (Supporterindex > -1){
-					SupporterList.splice(Supporterindex, 1);
-					alt.log(_L("log_supporterlist_del",[member.user.username]));
-				}
-			}
-			else if (isSupporter){
-				if (Supporterindex <= -1){
-					SupporterList.push(member.user.id);
-					alt.log(_L("log_supporterlist_add",[member.user.username]));
-				}
-			}
+			});
 		});
 	});
 }
-export function getIdByDiscord(dsid){
-  for (let i=0; i< onlineusers.length; i++){
-    if (dsid == onlineusers[i]["dsid"])
-      return onlineusers[i]["id"];
-  }
-  return -1;
-}
-export function getDiscordById(id){
-  for (let i=0; i< onlineusers.length; i++){
-    if (id == onlineusers[i]["id"])
-      return onlineusers[i]["dsid"];
+function getIdByDiscord(dsid){
+  for (let i=0; i< CurrentOnlineUsers.length; i++){
+    if (dsid == CurrentOnlineUsers[i].dsid)
+      return CurrentOnlineUsers[i].id;
   }
   return -1;
 }
 function getDiscordNameById(id){
-  for (let i=0; i< onlineusers.length; i++){
-    if (id == onlineusers[i]["id"])
-      return onlineusers[i]["username"];
+  for (let i=0; i< CurrentOnlineUsers.length; i++){
+    if (id == CurrentOnlineUsers[i].id)
+      return CurrentOnlineUsers[i].username;
   }
   return -1;
 }
 function getOnlineUsersByDsid(dsid){
-  for (let i=0; i< onlineusers.length; i++){
-    if (dsid == onlineusers[i]["dsid"])
+  for (let i=0; i< CurrentOnlineUsers.length; i++){
+    if (dsid == CurrentOnlineUsers[i].dsid)
       return i;
   }
   return -1;
 }
 function getOnlineUsersById(id){
-  for (let i=0; i< onlineusers.length; i++){
-    if (id == onlineusers[i]["id"])
+  for (let i=0; i< CurrentOnlineUsers.length; i++){
+    if (id == CurrentOnlineUsers[i].id)
       return i;
   }
   return -1;
 }
-export function isAdmin(id){
-	let server = discordClient.guilds.cache.get(_CONFIG.DISCORD_SERVER_ID);
-	server.members.fetch().then(members => {
-		members.forEach(member =>{
-			if (getDiscordById(id) != -1){
-				let sf_Admin = (element) => element == getDiscordById(id);
-				let Adminindex = AdminList.findIndex(sf_Admin);
-				return (Adminindex > -1)?true:false;
-			}
-			else
-				return false;
-		});
-	});
-}
-export function isMod(id){
-	let server = discordClient.guilds.cache.get(_CONFIG.DISCORD_SERVER_ID);
-	server.members.fetch().then(members => {
-		members.forEach(member =>{
-			if (getDiscordById(id) != -1){
-				let sf_Mod = (element) => element == getDiscordById(id);
-				let Modindex = ModList.findIndex(sf_Admin);
-				return (Modindex > -1)?true:false;
-			}
-			else
-				return false;
-		});
-	});
-}
-export function isSupporter(id){
-	let server = discordClient.guilds.cache.get(_CONFIG.DISCORD_SERVER_ID);
-	server.members.fetch().then(members => {
-		members.forEach(member =>{
-			if (getDiscordById(id) != -1){
-				let sf_Supporter = (element) => element == getDiscordById(id);
-				let Supporterindex = ModList.findIndex(sf_Supporter);
-				return (Supporterindex > -1)?true:false;
-			}
-			else
-				return false;
-		});
-	});
-}
 WhitelistUpdate = alt.setInterval(refreshWhitelist, 300000);
 alt.on('resourceStart', () => {
-    discordClient.login(_CONFIG.DISCORD_TOKEN).then(() => refreshWhitelist());
+    discordClient.login(process.env.DISCORD_TOKEN).then(() => refreshWhitelist());
 });
