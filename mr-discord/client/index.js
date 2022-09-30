0cfca84d936f6bac2c80da30560ce5c901b0d514
Luz Iferato <105192630+k37z3r@users.noreply.github.com>
2022-09-30 12:02:30 +0200

    Update index.js

--- mr-discord/client/index.js
+++ mr-discord/client/index.js
@@ -1,12 +1,24 @@
 import * as alt from 'alt';
+import * as native from 'natives';
+let view;
 async function getOAuthToken(DISCORD_APP_ID) {
     try {
         const token = await alt.Discord.requestOAuth2Token(DISCORD_APP_ID);
-        alt.emitServer('bao:discord:token', token);
+        alt.emitServer('mr-core:discord:token', token);
     } catch (e) {
         alt.logError(e);
     }
 }
-alt.onServer('bao:discord:set_discord_app_id', (APP_ID) => {
+native.freezeEntityPosition(alt.Player.local.scriptID, true);
+alt.onServer('mr-core:discord:set_discord_app_id', (APP_ID) => {
 	getOAuthToken(APP_ID);
+    alt.showCursor(true);
+	native.triggerScreenblurFadeIn(0);
+});
+alt.onServer('mr-core:discord:loggedin', (success) =>{
+	if (success == "success"){
+		native.triggerScreenblurFadeOut(0);
+		alt.showCursor(false);
+		native.freezeEntityPosition(alt.Player.local.scriptID, false);
+	}
 });
