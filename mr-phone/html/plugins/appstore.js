fcd09c84c9b67f695c5913262b7f07a82052681e
Luz Iferato <105192630+k37z3r@users.noreply.github.com>
2022-10-03 22:59:00 +0200

    Add file to mr-phone (read the README.md)

---
+++ mr-phone/html/plugins/appstore.js
@@ -0,0 +1,3 @@
+element = '<section class="phone_bgr_appstore_icon"><i class="fa-brands fa-app-store phone_bottom_icons phone_top_icons phone_appstore_icon"></i><section>';
+phone_top_apps_arr.push(element);
+insert_apps_to_start();
---
+++ mr-phone/html/plugins/sms.js
@@ -0,0 +1,3 @@
+element = '<div id="phone_sms"><i class="fa-solid fa-comment-sms phone_bottom_icons"></i></div>';
+phone_bottom_menu_arr.push(element);
+insert_bottom_menu();
