f016bb1ca904b07cef4793bd4e1e24c5dfcc595a
Luz Iferato <105192630+k37z3r@users.noreply.github.com>
2022-10-04 16:32:11 +0200

    Delete mr-phone/mr-phone directory

--- mr-phone/mr-phone/client/html/plugins/camera.js
+++
@@ -1,3 +0,0 @@
-element = '<div id="phone_camera"><i class="fa-solid fa-camera phone_bottom_icons"></i></div>';
-phone_bottom_menu_arr.push(element);
-insert_bottom_menu();
--- mr-phone/mr-phone/client/html/plugins/discord.js
+++
@@ -1,3 +0,0 @@
-element = '<div id="phone_discord"><i class="fa-brands fa-discord phone_bottom_icons"></i></div>';
-phone_bottom_menu_arr.push(element);
-insert_bottom_menu();
