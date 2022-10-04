388201c20346f50477d30cd3cb362ba27dc5d239
Luz Iferato <105192630+k37z3r@users.noreply.github.com>
2022-10-04 22:45:10 +0200

    Add some translations

--- mr-phone/server/index.js
+++ mr-phone/server/index.js
@@ -1,7 +1,9 @@
 import * as alt from "alt";
 import fs from "fs";
+import { _L } from 'mr-functions';
 var plugin_array = [];
 var path = "resources/mr-phone/client/html/plugins";
+var language_array = [_L("no_open_apps"), _L("description_snake"), _L("highscore"), _L("score"), _L("game_over"), _L("install"), _L("uninstall"), _L("open"), _L("call_ringtone"), _L("sms_ringtone"), _L("whatsapp_ringtone"), _L("whatsapp_msg_ringtone"), _L("email_ringtone"), _L("volume"), _L("keytones"), _L("listen_touched"), _L("see_touched"), _L("background_image"), _L("factory_defaults"), _L("do_you_have_defaults"), _L("untouchable_apps"), _L("yes"), _L("no"), _L("discord_msg_ringtone")];
 fs.readdir(path, function(err, files){
 	if (err)
 		console.log(err.message);
@@ -13,4 +15,5 @@ fs.readdir(path, function(err, files){
 });
 alt.onClient('mr-phone:get_scripts', player => {
     alt.emitClient(player, 'mr-phone:set_scripts', plugin_array);
+    alt.emitClient(player, 'mr-phone:set_language', language_array);
 });
