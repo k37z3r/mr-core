dc4802c56ba61b1da2ac9e96312ed704fa7cba4d
Luz Iferato <105192630+k37z3r@users.noreply.github.com>
2022-11-10 12:36:48 +0100

    replace spawn with pos

--- mr-iplloader/server/index.js
+++ mr-iplloader/server/index.js
@@ -1,5 +1,6 @@
 import * as alt from 'alt-server';
 import { getConfig } from 'mr-functions';
 alt.onClient("mr-iplloader:entry", (player, coords) => {
-	player.spawn(coords.x, coords.y, coords.z, coords.w);
+	let position = new alt.Vector3(coords.x, coords.y, coords.z);
+	player.pos = position;
 });
