f3d8f759855b3d0a41e098c1eb589f0ce4f0e5f2
Luz Iferato <105192630+k37z3r@users.noreply.github.com>
2022-11-10 13:17:33 +0100

    add tp2wp

--- mr-discord/client/index.js
+++ mr-discord/client/index.js
@@ -41,6 +41,7 @@ alt.onServer('mr-core:discord:getmessages', (messages) =>{
 alt.onServer('mr-core:discord:set_help_tp', (rule) => {
 	view.emit('set_help_tp', rule);
 });
+alt.onServer('teleportToMarker', tpToWaypoint)
 view.on('mr-core:discord:close_chat', close_chat);
 let opened = false;
 alt.on("keyup", (keycode) => {
@@ -73,4 +74,37 @@ function close_chat(){
 	alt.showCursor(false);
 	alt.toggleRmlControls(false);
 	view.unfocus();
+}
+function delay(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms)
+    })
+}
+let res = false
+let finalZ
+async function tpToWaypoint() {
+    res = false
+    const waypoint = native.getFirstBlipInfoId(8)
+    if (native.doesBlipExist(waypoint)) {
+        const coords = native.getBlipInfoIdCoord(waypoint)
+        let getZ = coords.z
+        while (!res || getZ > 1000) {
+            getZ = getZ + 20
+            const finalCoords = new alt.Vector3(coords.x, coords.y, getZ)
+            result(finalCoords)
+            await delay(1)
+        }
+        if (res) {
+            alt.emitServer('teleportToMarker', finalZ)
+        }
+        alt.FocusData.clearFocus()
+    }
+}
+function result(coords) {
+    alt.FocusData.overrideFocus(coords)
+    const result = native.getGroundZFor3dCoord(coords.x, coords.y,   coords.z, undefined, undefined, undefined)
+    if (result[0]) {
+        res = true
+        finalZ = new alt.Vector3(coords.x, coords.y, result[1] + 1)
+    }
 }
