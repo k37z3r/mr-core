884abe34d56fcb478587ee8956aaf06f86e5563c
Luz Iferato <105192630+k37z3r@users.noreply.github.com>
2022-12-19 13:18:17 +0100

    Update server.js

--- mr-toplist-updater/server.js
+++ mr-toplist-updater/server.js
@@ -1,25 +1,25 @@
 import * as alt from 'alt';
 import axios from 'axios';
-import { WHITELIST, SECRETPID, getConfig } from 'mr-functions';
+import { WHITELIST, SECRETPID, MINAGE, getConfig } from 'mr-functions';
 let TopListUpdate;
 async function doPostRequest() {
     let wl = "no";
     if (WHITELIST == "true")
-        wl = "yes";
+        wl = 'yes';
     else
-        wl = "no";
+        wl = 'no';
     var postdata = {
         minage: MINAGE,
         whitelist: wl,
-        language: getConfig(LANGUAGE),
-        framework: "mr-core",
-        voice: "Utilize alt:V builtin Voice",
-        chat: "ingame Discord",
+        language: getConfig("LANGUAGE"),
+        framework: 'mr-core',
+        voice: 'Utilize alt:V builtin Voice',
+        chat: 'ingame Discord',
         servername: alt.getServerConfig().name,
         serverip: alt.getServerConfig().host,
         serverport: alt.getServerConfig().port,
         gamemode: alt.getServerConfig().gamemode,
-        platform: "altv",
+        platform: 'altv',
         onlineplayers: alt.Player.all.length,
         maxplayers: alt.getServerConfig().players,
         secretToken: alt.getServerConfig().secretSID,
@@ -29,8 +29,8 @@ async function doPostRequest() {
             headers: {
                 'content-type': 'application/x-www-form-urlencoded'
             }
-        }
-        await axios.post('https://gta-spot.com/announce.php', postdata, headers)
+        };
+        await axios.post("https://gta-spot.com/announce.php", postdata, headers)
         .then(response => {
             if (response.data == "ok")
                 alt.log("data transfer success");
