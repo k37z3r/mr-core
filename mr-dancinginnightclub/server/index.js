7de2c8047ed75fc8986c8329281e4c5ca83e3f31
Luz Iferato <105192630+k37z3r@users.noreply.github.com>
2022-12-23 18:03:07 +0100

    Update index.js

--- mr-dancinginnightclub/server/index.js
+++ mr-dancinginnightclub/server/index.js
@@ -1,8 +1,5 @@
 import * as alt from "alt";
 import { getConfig } from 'mr-functions';
 alt.onClient("mr-iplloader:playerisconnected", (player) => {
-	let CONFIG = {
-		DancingPedAtNightclub: getConfig("Load_NightClubDancingPedAtNightclub"),
-	};
-	alt.emitClient(player, 'mr-iplloader:loadped',  CONFIG);
+    alt.emitClient(player, 'mr-iplloader:loadped',  getConfig("Load_NightClubDancingPedAtNightclub"));
 });
