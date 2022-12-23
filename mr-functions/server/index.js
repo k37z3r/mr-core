67748cf84fad1c849d1654d304ad827886a3697a
Luz Iferato <105192630+k37z3r@users.noreply.github.com>
2022-12-23 17:21:16 +0100

    Update index.js

--- mr-functions/server/index.js
+++ mr-functions/server/index.js
@@ -1,36 +1,38 @@
 import * as alt from 'alt-server';
 import mysql from 'mysql';
 import dotenv from 'dotenv';
+import axios from 'axios';
 import { _CONFIG } from '../config/config.js';
 import { translations } from '../language/language.js';
 dotenv.config();
 const neededValues = [
     'MYSQL_HOST',
-	'MYSQL_PORT',
+    'MYSQL_PORT',
     'MYSQL_USER',
     'MYSQL_PASSWORD',
     'MYSQL_DATABASE',
-	'DISCORD_APP_ID',
-	'DISCORD_SERVER_ID',
-	'DISCORD_TOKEN',
-	'DISCORD_CITIZEN_ID',
-	'DISCORD_ADMIN_ID',
-	'DISCORD_MOD_ID',
-	'DISCORD_SUPPORTER_ID',
-	'DISCORD_STATUS_CHANNEL',
-	'DISCORD_INGAME_CHANNEL',
-	'DISCORD_INVITE_LINK',
-	'DISCORD_PREFIX',
-	'WHITELIST',
-	'LOG_WHITELIST',
-	'LOG_MYSQL',
-	'ANNOUNCE_LOG_IN_OUT',
-	'DISCORD_ADMINCALL_CHANNEL',
-	'MINAGE',
-   	'SECRETPID',
-	'HUNGER_RATE',
-	'THIRSTY_RATE',
-	'UPDATE_INTERVAL'
+    'DISCORD_APP_ID',
+    'DISCORD_SERVER_ID',
+    'DISCORD_TOKEN',
+    'DISCORD_CITIZEN_ID',
+    'DISCORD_ADMIN_ID',
+    'DISCORD_MOD_ID',
+    'DISCORD_SUPPORTER_ID',
+    'DISCORD_STATUS_CHANNEL',
+    'DISCORD_INGAME_CHANNEL',
+    'DISCORD_INVITE_LINK',
+    'DISCORD_PREFIX',
+    'WHITELIST',
+    'LOG_WHITELIST',
+    'LOG_MYSQL',
+    'ANNOUNCE_LOG_IN_OUT',
+    'DISCORD_ADMINCALL_CHANNEL',
+    'MINAGE',
+    'LANGUAGE',
+    'SECRETPID',
+    'HUNGER_RATE',
+    'THIRSTY_RATE',
+    'UPDATE_INTERVAL'
 ];
 neededValues.forEach((value) => {
     if (!(value in process.env)) {
@@ -39,80 +41,80 @@ neededValues.forEach((value) => {
     }
 });
 const con = mysql.createConnection({
-	host: process.env.MYSQL_HOST,
-	port: process.env.MYSQL_PORT,
-	user: process.env.MYSQL_USER,
-	password: process.env.MYSQL_PASSWORD,
-	database: process.env.MYSQL_DATABASE
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 });
 
 
 let cmdHandlers = {};
 export function registerChatCmd(cmd, callback) {
-	cmd = cmd.toLowerCase();
-	if (cmdHandlers[cmd] !== undefined)
-		return false;
-	else
-		cmdHandlers[cmd] = callback;
+    cmd = cmd.toLowerCase();
+    if (cmdHandlers[cmd] !== undefined)
+        return false;
+    else
+        cmdHandlers[cmd] = callback;
 }
 export var canSplit = function(str, token){
     return (str || '').split(token).length > 1;         
 }
 export function invokeChatCmd(player, cmd, args) {
-	cmd = cmd.toLowerCase();
-	const callback = cmdHandlers[cmd];
-	if (callback)
-		callback(player, args);
-	else
-		return false;
+    cmd = cmd.toLowerCase();
+    const callback = cmdHandlers[cmd];
+    if (callback)
+        callback(player, args);
+    else
+        return false;
 }
 export async function mysql_callback(msql, values, cb){
-	if (msql.substr(0,6) == "INSERT"){
-		var connection = con.query(msql, values, function(err, result){
-			if (err){
-				throw err;
-				process.exit(0);
-			}
-			let numrows = 0;
-			numrows = result.affectedRows;
-			return cb([numrows, ((result[0]==undefined)?null:result[0])]);
-		});
-	}
-	else if (msql.substr(0,6) == "UPDATE"){
-		con.query(msql, values, function(err, result){
-			if (err){
-				throw err;
-				process.exit(0);
-			}
-			let numrows = 0;
-			numrows = result.changedRows;
-			return cb([numrows, ((result[0]==undefined)?null:result[0])]);
-		});
-	}
-	else{
-		var connection = con.query(msql, [values], function(err, result){
-			if (err){
-				throw err;
-				process.exit(0);
-			}
-			let numrows = 0;
-			if (msql.substr(0,6) == "SELECT")
-				numrows = result.length;
-			else
-				numrows = result.affectedRows;
-			return cb([numrows, ((result[0]==undefined)?null:result[0])]);
-		});
-	}
+    if (msql.substr(0,6) == "INSERT"){
+        var connection = con.query(msql, values, function(err, result){
+            if (err){
+                throw err;
+                process.exit(0);
+            }
+            let numrows = 0;
+            numrows = result.affectedRows;
+            return cb([numrows, ((result[0]==undefined)?null:result[0])]);
+        });
+    }
+    else if (msql.substr(0,6) == "UPDATE"){
+        con.query(msql, values, function(err, result){
+            if (err){
+                throw err;
+                process.exit(0);
+            }
+            let numrows = 0;
+            numrows = result.changedRows;
+            return cb([numrows, ((result[0]==undefined)?null:result[0])]);
+        });
+    }
+    else{
+        var connection = con.query(msql, [values], function(err, result){
+            if (err){
+                throw err;
+                process.exit(0);
+            }
+            let numrows = 0;
+            if (msql.substr(0,6) == "SELECT")
+                numrows = result.length;
+            else
+                numrows = result.affectedRows;
+            return cb([numrows, ((result[0]==undefined)?null:result[0])]);
+        });
+    }
 }
 export async function mysql_select_while_cb(msql, values, cb){
-	var connection = con.query(msql, [values], function(err, result){
-		if (err){
-			throw err;
-			process.exit(0);
-		}
-		let numrows = result.length;
-		return cb([numrows, ((result[0]==undefined)?null:result)]);
-	});
+    var connection = con.query(msql, [values], function(err, result){
+        if (err){
+            throw err;
+            process.exit(0);
+        }
+        let numrows = result.length;
+        return cb([numrows, ((result[0]==undefined)?null:result)]);
+    });
 }
 export const DISCORD_APP_ID = process.env.DISCORD_APP_ID;
 export const DISCORD_SERVER_ID = process.env.DISCORD_SERVER_ID;
@@ -136,42 +138,42 @@ export const HUNGER_RATE = process.env.HUNGER_RATE;
 export const THIRSTY_RATE = process.env.THIRSTY_RATE;
 export const UPDATE_INTERVAL = process.env.UPDATE_INTERVAL;
 export function getConfig(value){
-	for(var key in _CONFIG) {
-		if(key == value) {
-			return _CONFIG[key];
-		}
-	}
-	return 0;
+    for(var key in _CONFIG) {
+        if(key == value) {
+            return _CONFIG[key];
+        }
+    }
+    return 0;
 }
 export function _L(trans = "", opts="", lang = _CONFIG.LANGUAGE){
-	var i = 0;
-	var str;
-	for (var key in translations){
-		if (key == lang){
-			str = translations[key][trans];
-			str = (str || key+'.'+trans);
-			for (var opt in opts){
-				var search = "%s_" + opt;
-				str = str.replaceAll(search, opts[opt])
-			}
-		return str;
-		i++;
-		}
-	}
-	if (i==0)
-		return lang+'.'+trans;
+    var i = 0;
+    var str;
+    for (var key in translations){
+        if (key == lang){
+            str = translations[key][trans];
+            str = (str || key+'.'+trans);
+            for (var opt in opts){
+                var search = "%s_" + opt;
+                str = str.replaceAll(search, opts[opt])
+            }
+        return str;
+        i++;
+        }
+    }
+    if (i==0)
+        return lang+'.'+trans;
 }
 export function get_dsid(disid, cb){
-	var msql = 'SELECT * FROM users WHERE id = ?';
-	var connection = con.query(msql, [disid.id], function(err, result){
-		if (err){
-			throw err;
-			process.exit(0);
-		}
-		let numrows = 0;
-		numrows = result.length;
-		return cb([numrows, ((result[0]==undefined)?null:result[0].dsid)]);
-	});
+    var msql = 'SELECT * FROM users WHERE users.id = ?';
+    var connection = con.query(msql, [disid.id], function(err, result){
+        if (err){
+            throw err;
+            process.exit(0);
+        }
+        let numrows = 0;
+        numrows = result.length;
+        return cb([numrows, ((result[0]==undefined)?null:result[0].dsid)]);
+    });
 }
 export function sortFunction(a, b) {
     if (a[0] === b[0]) {
@@ -180,4 +182,48 @@ export function sortFunction(a, b) {
     else {
         return (a[0] < b[0]) ? -1 : 1;
     }
+}
+export async function doPostRequest() {
+    let wl = "no";
+    if (WHITELIST == "true")
+        wl = "yes";
+    else
+        wl = "no";
+    var postdata = {
+        minage: MINAGE,
+        whitelist: wl,
+        language: getConfig(LANGUAGE),
+        framework: "mr-core",
+        voice: "Utilize alt:V builtin Voice",
+        chat: "ingame Discord",
+        servername: alt.getServerConfig().name,
+        serverip: alt.getServerConfig().host,
+        serverport: alt.getServerConfig().port,
+        gamemode: alt.getServerConfig().gamemode,
+        platform: "altv",
+        onlineplayers: alt.Player.all.length,
+        maxplayers: alt.getServerConfig().players,
+        secretToken: alt.getServerConfig().secretSID,
+        secretToken2: SECRETPID
+    };
+    var headers = {
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded'
+        }
+    }
+    await axios.post('https://gta-spot.com/announce.php', postdata, headers)
+    .then(response => {
+        if (response.data == "ok")
+            alt.log("data transfer success");
+        else
+            alt.log(response.data);
+    })
+    .catch(error => {
+        if (error.response)
+            alt.log(error.response.status);
+        else if (error.request)
+            alt.log(error.request);
+        else
+            alt.log(error.message);
+    });
 }
