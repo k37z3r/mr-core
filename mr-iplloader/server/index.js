815c754820061463c6efd5f8fe07237f39d8f0b9
Luz Iferato <105192630+k37z3r@users.noreply.github.com>
2022-12-23 18:10:22 +0100

    Update index.js

--- mr-iplloader/server/index.js
+++ mr-iplloader/server/index.js
@@ -1,6 +1,5 @@
 import * as alt from 'alt-server';
-import { getConfig } from 'mr-functions';
 alt.onClient("mr-iplloader:entry", (player, coords) => {
-	let position = new alt.Vector3(coords.x, coords.y, coords.z);
-	player.pos = position;
+    let position = new alt.Vector3(coords.x, coords.y, coords.z);
+    player.pos = position;
 });
