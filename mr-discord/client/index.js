1f5bfb6e881e17340d471c0e420e6b75b3f1e13f
Luz Iferato <105192630+k37z3r@users.noreply.github.com>
2022-12-23 17:42:59 +0100

    Update index.js

--- mr-discord/client/index.js
+++ mr-discord/client/index.js
@@ -13,67 +13,67 @@ async function getOAuthToken(DISCORD_APP_ID) {
 }
 native.freezeEntityPosition(alt.Player.local.scriptID, true);
 alt.onServer('mr-core:discord:set_discord_app_id', (APP_ID) => {
-	getOAuthToken(APP_ID);
+    getOAuthToken(APP_ID);
     alt.showCursor(true);
-	native.triggerScreenblurFadeIn(0);
+    native.triggerScreenblurFadeIn(0);
 });
 alt.onServer('mr-core:discord:loggedin', (success) =>{
-	if (success == "success"){
-		native.triggerScreenblurFadeOut(0);
-		alt.showCursor(false);
-		native.freezeEntityPosition(alt.Player.local.scriptID, false);
-	}
+    if (success == "success"){
+        native.triggerScreenblurFadeOut(0);
+        alt.showCursor(false);
+        native.freezeEntityPosition(alt.Player.local.scriptID, false);
+    }
 });
 alt.on("connectionComplete", () => {
-	alt.emitServer('mr-core:discord:loadmessages');
+    alt.emitServer('mr-core:discord:loadmessages');
 });
 alt.onServer('mr-core:discord:setmessages', (messages) =>{
-	view.emit("setMessages", messages);
+    view.emit("setMessages", messages);
 });
 view.on('mr-core:discord:sendmessage', sendmessage);
 function sendmessage(msg){
-	msg = msg.replace('<div><br></div>', '');
-	alt.emitServer('mr-core:discord:msgtodiscord', msg);
+    msg = msg.replace('<div><br></div>', '');
+    alt.emitServer('mr-core:discord:msgtodiscord', msg);
 }
 alt.onServer('mr-core:discord:getmessages', (messages) =>{
-	alt.emitServer("mr-core:discord:loadmessages");
+    alt.emitServer("mr-core:discord:loadmessages");
 });
 alt.onServer('mr-core:discord:set_help', (rule) => {
-	view.emit('set_help', rule);
+    view.emit('set_help', rule);
 });
 alt.onServer('teleportToMarker', tpToWaypoint)
 view.on('mr-core:discord:close_chat', close_chat);
 let opened = false;
 alt.on("keyup", (keycode) => {
-	switch (keycode) {
-		case 84: // Key: T
-			if (!opened && alt.gameControlsEnabled()){
-				open_chat();
-			}
-		break;
-		case 27: // Key: escape
-			if (opened && !alt.gameControlsEnabled()){
-				close_chat();
-			}
-		break;
-	}
+    switch (keycode) {
+        case 84: // Key: T
+            if (!opened && alt.gameControlsEnabled()){
+                open_chat();
+            }
+        break;
+        case 27: // Key: escape
+            if (opened && !alt.gameControlsEnabled()){
+                close_chat();
+            }
+        break;
+    }
 });
 function open_chat(){
-	alt.emitServer('mr-core:discord:set_help');
-	opened = true;
-	view.emit("openChat", lang_array);
-	alt.toggleGameControls(false);
-	alt.showCursor(true);
-	alt.toggleRmlControls(true);
-	view.focus();
+    alt.emitServer('mr-core:discord:set_help');
+    opened = true;
+    view.emit("openChat", lang_array);
+    alt.toggleGameControls(false);
+    alt.showCursor(true);
+    alt.toggleRmlControls(true);
+    view.focus();
 }
 function close_chat(){
-	opened = false;
-	view.emit("closeChat");
-	alt.toggleGameControls(true);
-	alt.showCursor(false);
-	alt.toggleRmlControls(false);
-	view.unfocus();
+    opened = false;
+    view.emit("closeChat");
+    alt.toggleGameControls(true);
+    alt.showCursor(false);
+    alt.toggleRmlControls(false);
+    view.unfocus();
 }
 function delay(ms) {
     return new Promise((resolve) => {
