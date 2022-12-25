50eb06ddbdf20faaa45d1bd9e3c1eea0dfc28c65
Luz Iferato <105192630+k37z3r@users.noreply.github.com>
2022-12-25 17:40:59 +0100

    little bug fixed

--- mr-iplloader/client/garage5.mjs
+++ mr-iplloader/client/garage5.mjs
@@ -1,6 +1,5 @@
 import * as native from 'natives';
 import { EnableIpl, SetIplProp, getConfig } from 'mr-functions';
-import { getConfig } from '../../mr-functions/client';
 export let Garage5 = {
     ipl : ["xm3_int_placement_xm3_interior_1_dlc_int_01_xm3_milo_"],
     Enable : function(state){
