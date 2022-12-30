2974ae8045cca5955f14bad9471c719aecb7cf5c
Luz Iferato <105192630+k37z3r@users.noreply.github.com>
2022-12-30 14:26:10 +0100

    Update index.js

--- mr-functions/client/index.js
+++ mr-functions/client/index.js
@@ -3,9 +3,20 @@ import * as native from 'natives';
 import { _CONFIG } from '../config/config.js';
 import { translations } from '../language/language.js';
 const view = new alt.WebView("http://resource/client/html/index.html");
+let pois = [];
+let poi_id = [];
+var hudopened = false;
+let keymappingopened = false;
+let res = false;
+let finalZ;
 alt.setWatermarkPosition(2);
-export function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+function result(coords) {
+    alt.FocusData.overrideFocus(coords);
+    const result = native.getGroundZFor3dCoord(coords.x, coords.y,   coords.z, undefined, undefined, undefined);
+    if (result[0]) {
+        res = true;
+        finalZ = new alt.Vector3(coords.x, coords.y, result[1] + 1)
+    }
 }
 export const RequestModel = async (modelHash, timeoutMs = 10000) => {
     return new Promise((resolve, reject) => {
@@ -53,6 +64,9 @@ export const RequestAnimDict = async (Anim, timeoutMs = 10000) => {
         }, 100);
     });
 };
+export function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
 export function EnableIpl(ipl, activate){
     let key;
     for (key in ipl){
@@ -77,7 +91,7 @@ export function setBlip(x,y,z, sprite, scale, color, sndcolor, seccolor, name){
     native.endTextCommandSetBlipName(Blip);
 }
 export function SetPropColor(id, prop, style){
-    native.setInteriorEntitySetColor(id, prop, style);
+    native.setInteriorEntitySetTintIndex(id, prop, style);
     native.refreshInterior(id);
 }
 export function getInteriorByType(x, y, z, name, iplName){
@@ -91,7 +105,6 @@ export function SetIplProp(interiorId, props, active){
         native.activateInteriorEntitySet(interiorId, props);
     else
         native.deactivateInteriorEntitySet(interiorId, props);
-    native.refreshInterior(interiorId);
 }
 export function UnSetIplProp(interiorId, props){
     let key;
@@ -121,8 +134,6 @@ export function drawTv() {
     native.enableMovieSubtitles(true);
     return true;
 }
-let pois = [];
-let poi_id = [];
 export function RegisterPoi(id, posX, posY, posZ, maxDistance, txt){
     if (poi_id.indexOf(id) === -1){
         poi_id.push(id);
@@ -131,15 +142,15 @@ export function RegisterPoi(id, posX, posY, posZ, maxDistance, txt){
     let key;
     for (key in pois){
         if(!pois[key][2]){
-            view.emit("setText", pois[key][0], format_txt(pois[key][1]));
+            view.emit("mr-core:functions:setText", pois[key][0], format_txt(pois[key][1]));
             pois[key][2] = true;
         }
         if (pois[key][3] == false && alt.Player.local.pos.distanceTo(new alt.Vector3(pois[key][4], pois[key][5], pois[key][6])) < pois[key][7]){
-                view.emit("drawText", pois[key][0]);
+                view.emit("mr-core:functions:drawText", pois[key][0]);
                 pois[key][3] = true;
         }
         if (pois[key][3] == true && alt.Player.local.pos.distanceTo(new alt.Vector3(pois[key][4], pois[key][5], pois[key][6])) > pois[key][7]){
-                view.emit("hideText", pois[key][0]);
+                view.emit("mr-core:functions:hideText", pois[key][0]);
                 pois[key][3] = false;
         }
     }
@@ -169,7 +180,7 @@ export function format_txt(txt){
     str = str.replaceAll('[190]', replace_buttons('.'));
     return str;
 }
-function create_help(){
+export function create_help(){
       var html = `<table><tr>`;
       var i = 0;
       for (const [key, value] of Object.entries(getConfig("HELP"))) {
@@ -214,13 +225,80 @@ export function _L(trans = "", opts="", lang = _CONFIG.LANGUAGE){
 export function doUpdateHud(){
     alt.emitServer('mr-core:hud:updateHUD');
 }
-let keymappingopened = false;
-view.emit("setkeymapping",create_help());
+export function doUpdateRunning(){
+    let pisdead = native.isPlayerDead(alt.Player.local.scriptID);
+    let parmour = native.getPedArmour(alt.Player.local.scriptID);
+    let phealth = (native.getEntityHealth(alt.Player.local.scriptID)/2);
+    let pstamina = (100 - native.getPlayerSprintStaminaRemaining(alt.Player.local.scriptID));
+    let poxygen = (native.getPlayerUnderwaterTimeRemaining(alt.Player.local.scriptID) * 20);
+    if ((phealth < 20 || poxygen < 20 || pisdead) && !hudopened){
+        alt.emit('mr-core:hud:openhud');
+        hudopened = true;
+    }
+    alt.emit('mr-core:hud:set_running', {health: phealth, running: pstamina, oxygen: poxygen, shield: parmour});
+}
+export async function getOAuthToken(DISCORD_APP_ID) {
+    try {
+        const token = await alt.Discord.requestOAuth2Token(DISCORD_APP_ID);
+        alt.emitServer('mr-core:discord:token', token);
+    } catch (e) {
+        alt.logError(e);
+    }
+}
+export function sendmessage(msg){
+    msg = msg.replace('<div><br></div>', '');
+    alt.emitServer('mr-core:discord:msgtodiscord', msg);
+}
+export function delay(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms)
+    })
+}
+export function open_chat(){
+    const lang_array = [_L("TELEPORT_TO_COORDS"), _L("TELEPORT_TO_PLAYER"), _L("CALL_ADMIN"), _L("TELEPORT_TO_WAYPOINT"), _L("SPAWN_VEHICLE")];
+    alt.emitServer('mr-core:discord:set_help');
+    alt.emit('mr-core:discord:openchat', lang_array);
+    alt.toggleGameControls(false);
+    if (!alt.isCursorVisible()){
+        alt.showCursor(true);
+    }
+    alt.toggleRmlControls(true);
+}
+export function close_chat(){
+    alt.emit('mr-core:discord:closechat');
+    alt.toggleGameControls(true);
+    if (alt.isCursorVisible()){
+        alt.showCursor(false);
+    }
+    alt.toggleRmlControls(false);
+}
+export async function tpToWaypoint() {
+    res = false;
+    const waypoint = native.getFirstBlipInfoId(8);
+    if (native.doesBlipExist(waypoint)) {
+        const coords = native.getBlipInfoIdCoord(waypoint);
+        let getZ = coords.z;
+        while (!res || getZ > 1000) {
+            getZ = getZ + 20;
+            const finalCoords = new alt.Vector3(coords.x, coords.y, getZ);
+            result(finalCoords);
+            await delay(1);
+        }
+        if (res) {
+            alt.emitServer('mr-core:discord:teleportToMarker', finalZ);
+        }
+        alt.FocusData.clearFocus();
+    }
+}
+alt.on('mr-core:functions:hudopened', msg => {
+    hudopened = msg;
+});
+view.emit("mr-core:functions:setkeymapping",create_help());
 alt.on("keyup", (keycode) => {
     switch (keycode) {
         case 121: // Key: F10
             if (!keymappingopened && alt.gameControlsEnabled()){
-                view.emit("openkeymapping");
+                view.emit("mr-core:functions:openkeymapping");
                 keymappingopened = true;
                 alt.toggleGameControls(false);
                 alt.showCursor(true);
@@ -228,7 +306,7 @@ alt.on("keyup", (keycode) => {
                 view.focus();
             }
             else if (keymappingopened && !alt.gameControlsEnabled()){
-                view.emit("closekeymapping");
+                view.emit("mr-core:functions:closekeymapping");
                 keymappingopened = false;
                 alt.toggleGameControls(true);
                 alt.showCursor(false);
@@ -238,7 +316,7 @@ alt.on("keyup", (keycode) => {
         break;
         case 27: // Key: esacape
             if (keymappingopened && !alt.gameControlsEnabled()){
-                view.emit("closekeymapping");
+                view.emit("mr-core:functions:closekeymapping");
                 keymappingopened = false;
                 alt.toggleGameControls(true);
                 alt.showCursor(false);
