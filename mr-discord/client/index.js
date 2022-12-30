97b2a007914c513a49bbca5536b79127fecd7201
Luz Iferato <105192630+k37z3r@users.noreply.github.com>
2022-12-30 14:28:43 +0100

    Update index.js

--- mr-discord/client/index.js
+++ mr-discord/client/index.js
@@ -1,110 +1,64 @@
 import * as alt from 'alt';
 import * as native from 'natives';
-import { _L } from 'mr-functions';
+import { getOAuthToken, sendmessage, open_chat, close_chat, tpToWaypoint } from 'mr-functions';
 const view = new alt.WebView("http://resource/client/html/index.html");
-const lang_array = [_L("TELEPORT_TO_COORDS"), _L("TELEPORT_TO_PLAYER"), _L("CALL_ADMIN"), _L("TELEPORT_TO_WAYPOINT"), _L("SPAWN_VEHICLE")];
-async function getOAuthToken(DISCORD_APP_ID) {
-    try {
-        const token = await alt.Discord.requestOAuth2Token(DISCORD_APP_ID);
-        alt.emitServer('mr-core:discord:token', token);
-    } catch (e) {
-        alt.logError(e);
-    }
-}
 native.freezeEntityPosition(alt.Player.local.scriptID, true);
 alt.onServer('mr-core:discord:set_discord_app_id', (APP_ID) => {
     getOAuthToken(APP_ID);
     alt.showCursor(true);
     native.triggerScreenblurFadeIn(0);
 });
-alt.onServer('mr-core:discord:loggedin', (success) =>{
-    if (success == "success"){
+alt.onServer('mr-core:discord:loggedin', (data) =>{
+    if (data.status == "success"){
         native.triggerScreenblurFadeOut(0);
         alt.showCursor(false);
         native.freezeEntityPosition(alt.Player.local.scriptID, false);
     }
 });
+alt.on('mr-core:discord:openchat', (lang_array) =>{
+    view.emit("mr-core:discord:openChat", lang_array);
+    view.focus();
+});
+alt.on('mr-core:discord:closechat', () =>{
+    view.emit("mr-core:discord:closeChat");
+    view.unfocus();
+});
 alt.on("connectionComplete", () => {
     alt.emitServer('mr-core:discord:loadmessages');
 });
 alt.onServer('mr-core:discord:setmessages', (messages) =>{
-    view.emit("setMessages", messages);
+    view.emit("mr-core:discord:setMessages", messages);
 });
 view.on('mr-core:discord:sendmessage', sendmessage);
-function sendmessage(msg){
-    msg = msg.replace('<div><br></div>', '');
-    alt.emitServer('mr-core:discord:msgtodiscord', msg);
-}
 alt.onServer('mr-core:discord:getmessages', (messages) =>{
     alt.emitServer("mr-core:discord:loadmessages");
 });
 alt.onServer('mr-core:discord:set_help', (rule) => {
-    view.emit('set_help', rule);
+    view.emit('mr-core:discord:set_help', rule);
 });
-alt.onServer('teleportToMarker', tpToWaypoint)
-view.on('mr-core:discord:close_chat', close_chat);
+alt.onServer('mr-core:discord:teleportToMarker', tpToWaypoint);
+alt.onServer('mr-core:discord:close_chat', () => {
+    opened = false;
+    close_chat();
+});
+view.on('mr-core:discord:close_chat', () => {
+    opened = false;
+    close_chat();
+});
 let opened = false;
 alt.on("keyup", (keycode) => {
     switch (keycode) {
         case 84: // Key: T
             if (!opened && alt.gameControlsEnabled()){
+                opened = true;
                 open_chat();
             }
         break;
         case 27: // Key: escape
             if (opened && !alt.gameControlsEnabled()){
+                opened = false;
                 close_chat();
             }
         break;
     }
 });
-function open_chat(){
-    alt.emitServer('mr-core:discord:set_help');
-    opened = true;
-    view.emit("openChat", lang_array);
-    alt.toggleGameControls(false);
-    alt.showCursor(true);
-    alt.toggleRmlControls(true);
-    view.focus();
-}
-function close_chat(){
-    opened = false;
-    view.emit("closeChat");
-    alt.toggleGameControls(true);
-    alt.showCursor(false);
-    alt.toggleRmlControls(false);
-    view.unfocus();
-}
-function delay(ms) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, ms)
-    })
-}
-let res = false
-let finalZ
-async function tpToWaypoint() {
-    res = false
-    const waypoint = native.getFirstBlipInfoId(8)
-    if (native.doesBlipExist(waypoint)) {
-        const coords = native.getBlipInfoIdCoord(waypoint)
-        let getZ = coords.z
-        while (!res || getZ > 1000) {
-            getZ = getZ + 20
-            const finalCoords = new alt.Vector3(coords.x, coords.y, getZ)
-            result(finalCoords)
-            await delay(1)
-        }
-        if (res) {
-            alt.emitServer('teleportToMarker', finalZ)
-        }
-        alt.FocusData.clearFocus()
-    }
-}
-function result(coords) {
-    alt.FocusData.overrideFocus(coords)
-    const result = native.getGroundZFor3dCoord(coords.x, coords.y,   coords.z, undefined, undefined, undefined)
-    if (result[0]) {
-        res = true
-        finalZ = new alt.Vector3(coords.x, coords.y, result[1] + 1)
-    }
-}
