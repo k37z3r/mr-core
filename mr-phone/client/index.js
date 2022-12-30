d2bfce26c0986bc69860de69423a60181852c401
Luz Iferato <105192630+k37z3r@users.noreply.github.com>
2022-12-30 16:36:45 +0100

    Update index.js

--- mr-phone/client/index.js
+++ mr-phone/client/index.js
@@ -1,40 +1,31 @@
 import * as alt from "alt";
+import { close_phone, open_phone } from 'mr-functions';
 let opened = false;
 const view = new alt.WebView("http://resource/client/html/index.html");
 alt.on("keyup", (keycode) => {
     switch (keycode) {
         case 77: // Key: M
             if (!opened && alt.gameControlsEnabled()){
+                opened = true;
+                view.emit("mr-core:phone:openPhone");
+                view.focus();
                 open_phone();
             }
         break;
         case 27: // Key: escape
             if (opened && !alt.gameControlsEnabled()){
+                opened = false;
+                view.emit("mr-core:phone:closePhone");
+                view.unfocus();
                 close_phone();
             }
         break;
     }
 });
-function close_phone(){
-    opened = false;
-    view.emit("closePhone");
-    alt.toggleGameControls(true);
-    alt.showCursor(false);
-    alt.toggleRmlControls(false);
-    view.unfocus();
-}
-function open_phone(){
-    opened = true;
-    view.emit("openPhone");
-    alt.toggleGameControls(false);
-    alt.showCursor(true);
-    alt.toggleRmlControls(true);
-    view.focus();
-}
-alt.emitServer("mr-phone:get_scripts");
-alt.onServer('mr-phone:set_scripts', (p_array) => {
-    view.emit("load_res_plugins", p_array);
+alt.emitServer("mr-core:phone:get_scripts");
+alt.onServer('mr-core:phone:set_scripts', (p_array) => {
+    view.emit("mr-core:phone:load_res_plugins", p_array);
 });
-alt.onServer('mr-phone:set_language', (p_array) => {
-    view.emit("load_language", p_array);
+alt.onServer('mr-core:phone:set_language', (p_array) => {
+    view.emit("mr-core:phone:load_language", p_array);
 });
